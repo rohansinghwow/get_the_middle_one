28ae4fa9ed8d8eefaf77e2a348baa14f69024e1d
rohansinghwow <samfrankie439@gmail.com>
2021-03-03 23:48:48 +0530

    Used ternary operator for simplifying the code

--- middle.js
+++ middle.js
@@ -1,5 +1,5 @@
 function middleOne(myStr){
-    return (myStr.length%2 === 0 ? myStr[(myStr.length/2 - 1)] + myStr[(myStr.length/2)] : myStr[Math.floor(myStr.length/2)]); //Used to check if string length is Odd or Even.
+    return (myStr.length%2 === 0 ? myStr[(myStr.length/2 - 1)] + myStr[(myStr.length/2)] : myStr[Math.floor(myStr.length/2)]); //Used to check if string length is Odd or Even. Then perform the check using ternary operator
   }
   
   console.log("The middle string is  : " + middleOne("qwerty"));
