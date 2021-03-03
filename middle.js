88375c2e4450b4ea38c68b49b1821c6e2d620b68
rohansinghwow <samfrankie439@gmail.com>
2021-03-03 23:45:21 +0530

    Added a program for output of middle string

---
+++ middle.js
@@ -0,0 +1,5 @@
+function middleOne(myStr){
+    return (myStr.length%2 === 0 ? myStr[(myStr.length/2 - 1)] + myStr[(myStr.length/2)] : myStr[Math.floor(myStr.length/2)]); //Used to check if string length is Odd or Even.
+  }
+  
+  console.log("The middle string is  : " + middleOne("qwerty"));
